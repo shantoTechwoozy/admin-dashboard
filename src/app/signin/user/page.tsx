69ee0d2594c5e3f2b859e652738053528ff88a4f
nasim <nasim.helal.techwoozy@gmail.com>
2024-09-14 16:41:18 +0600

    vercel deployment error solved

--- src/app/signin/user/page.tsx
+++ src/app/signin/user/page.tsx
@@ -1,26 +1,31 @@
-"use client"
+"use client";
 import LoginForm from "@/components/common/auth/LoginForm";
 import LoginFormFooter from "@/components/common/auth/LoginFormFooter";
 import { useState } from "react";
 
 const UserLoginPage = () => {
-    //agentID means userName
-    const [loginData, setLoginData] = useState({ agentID: "", password: "" })
+  //agentID means userName
+  const [loginData, setLoginData] = useState({ agentID: "", password: "" });
 
-    console.log("Data fetched", loginData)
+  console.log("Data fetched", loginData);
 
-    return (
-        <div className="w-screen min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-800 px-4 sm:px-6 lg:px-8">
-            <div className="relative py-3 sm:w-115 sm:mx-auto">
-                <div className="flex justify-center items-center min-h-90 text-left bg-white dark:bg-gray-900 rounded-[50px] shadow-lg">
-                    <div className="flex flex-col justify-center items-center h-full">
-                        <LoginForm title="User Login" loginData={loginData} setLoginData={setLoginData} />
-                        <LoginFormFooter name="User" registration="Register" />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="bg-gray-50 dark:bg-gray-800 flex min-h-screen w-screen items-center justify-center px-4 sm:px-6 lg:px-8">
+      <div className="relative py-3 sm:mx-auto sm:w-115">
+        <div className="dark:bg-gray-900 flex min-h-90 items-center justify-center rounded-[50px] bg-white text-left shadow-lg">
+          <div className="flex h-full flex-col items-center justify-center">
+            <LoginForm
+              title="User Login"
+              loginData={loginData}
+              setLoginData={setLoginData}
+              onSubmit={() => {}}
+            />
+            <LoginFormFooter name="User" registration="Register" />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default UserLoginPage;
