dee0ed4fd0f6e1338e0cd9629cfd03617531bfc2
shantoTechwoozy <shanto.techwoozy@gmail.com>
2024-09-15 18:25:57 +0600

    Redux slice for filter created

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import "@/css/satoshi.css";
 import "@/css/style.css";
+import { store } from "@/redux/store";
 import "flatpickr/dist/flatpickr.min.css";
 import "jsvectormap/dist/jsvectormap.css";
 import React from "react";
+import { Provider } from "react-redux";
 
 export default function RootLayout({
   children,
@@ -12,7 +14,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body suppressHydrationWarning={true}>
-        <main className="dark:bg-boxdark-2 dark:text-bodydark">{children}</main>
+        <main className="dark:bg-boxdark-2 dark:text-bodydark">
+          <Provider store={store}>
+            {children}
+          </Provider>
+        </main>
       </body>
     </html>
   );
